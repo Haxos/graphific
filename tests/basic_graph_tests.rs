9fdb7117f73895d452e20ded67f4627237801ef8
Haxos <a.mayocartes@protonmail.com>
2021-02-04 16:58:35 +0100

    added test basic graphs

---
+++ tests/basic_graph_tests.rs
@@ -0,0 +1,93 @@
+extern crate graphific;
+
+#[cfg(test)]
+#[macro_use]
+extern crate pretty_assertions;
+
+#[cfg(test)]
+mod basic_directed_graph_tests {
+
+    #[test]
+    fn new_basic_directed_graph() {
+        unimplemented!()
+    }
+
+    #[test]
+    fn add_vertex() {
+        unimplemented!()
+    }
+
+    #[test]
+    fn remove_vertex() {
+        unimplemented!()
+    }
+
+    #[test]
+    fn add_edge() {
+        unimplemented!()
+    }
+
+    #[test]
+    fn remove_edge() {
+        unimplemented!()
+    }
+
+    #[test]
+    fn remove_all_edges() {
+        unimplemented!()
+    }
+
+    #[test]
+    fn successors() {
+        unimplemented!()
+    }
+
+    #[test]
+    fn predecessors() {
+        unimplemented!()
+    }
+}
+
+#[cfg(test)]
+mod basic_undirected_graph_tests {
+
+    #[test]
+    fn new_basic_directed_graph() {
+        unimplemented!()
+    }
+
+    #[test]
+    fn add_vertex() {
+        unimplemented!()
+    }
+
+    #[test]
+    fn remove_vertex() {
+        unimplemented!()
+    }
+
+    #[test]
+    fn add_edge() {
+        unimplemented!()
+    }
+
+    #[test]
+    fn remove_edge() {
+        unimplemented!()
+    }
+
+    #[test]
+    fn remove_all_edges() {
+        unimplemented!()
+    }
+
+    #[test]
+    fn successors() {
+        unimplemented!()
+    }
+
+    #[test]
+    fn predecessors() {
+        unimplemented!()
+    }
+}
