7dc618f1a2ccdc2343488ef4ae8a91e63c6c4761
Haxos <a.mayocartes@protonmail.com>
2021-02-05 15:50:51 +0100

    tested remove_vertex of BDG

--- tests/basic_graph_tests.rs
+++ tests/basic_graph_tests.rs
@@ -41,7 +41,46 @@ mod basic_directed_graph_tests {
 
     #[test]
     fn remove_vertex() {
-        unimplemented!()
+        let mut graph: BasicDirectedGraph<i32, i32> = BasicDirectedGraph::new();
+        let mut vertices: Vec<Vertex<i32, i32>> = vec![];
+        let v1: Vertex<i32, i32> = Vertex::with_value(0, 1);
+        let v2: Vertex<i32, i32> = Vertex::with_value(1, 2);
+        let e1: Edge<i32> = Edge::new(v1.key().clone(), v2.key().clone());
+
+        graph = graph.add_vertex(v1.clone()).unwrap();
+        graph = graph.add_vertex(v2.clone()).unwrap();
+        graph = graph.add_edge(e1.clone()).unwrap();
+        vertices.push(v1.clone());
+        vertices.push(v2.clone());
+
+        // initial check
+        assert_eq!(vertices, graph.vertices());
+
+        // remove v1
+        let (graph, removed_v1, removed_edges) = graph.remove_vertex(&v1).unwrap();
+        let excepted_removed_edges: Vec<Edge<i32>> = vec![e1];
+        let excepted_remaining_edges: Vec<Edge<i32>> = vec![];
+        vertices = vec![v2.clone()];
+
+        assert_eq!(vertices, graph.vertices());
+        assert_eq!(excepted_remaining_edges, graph.edges());
+        assert_eq!(v1, removed_v1);
+        assert_eq!(excepted_removed_edges, removed_edges);
+
+        // try to remove v1 but fail
+        let should_be_none = graph.remove_vertex(&v1);
+        assert_eq!(true, should_be_none.is_none());
+
+        // remove v2
+        let (graph, removed_v2, removed_edges) = graph.remove_vertex(&v2).unwrap();
+        let excepted_removed_edges: Vec<Edge<i32>> = vec![];
+        let excepted_remaining_edges: Vec<Edge<i32>> = vec![];
+        vertices = vec![];
+
+        assert_eq!(vertices, graph.vertices());
+        assert_eq!(excepted_remaining_edges, graph.edges());
+        assert_eq!(v2, removed_v2);
+        assert_eq!(excepted_removed_edges, removed_edges);
     }
 
     #[test]
