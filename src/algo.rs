28a0cd7cd8699c4cf01657e0048f213e5fbaeef1
Haxos <a.mayocartes@protonmail.com>
2021-02-09 20:26:36 +0100

    added dfs

--- src/algo.rs
+++ src/algo.rs
@@ -46,4 +46,45 @@ where
             return None;
         };
     }
+
+    fn dfs(&self) -> Option<Self> {
+        return if self.vertices().is_empty() {
+            None
+        } else {
+            let first = self.vertices().first().unwrap().clone();
+            self.dfs_with_starting_vertex(&first)
+        };
+    }
+
+    fn dfs_with_starting_vertex(&self, starting_vertex: &Vertex<K, V>) -> Option<Self> {
+        if !self.vertices().contains(starting_vertex) {
+            return None;
+        }
+        let new_graph = self.clone();
+        let mut stack: VecDeque<K> = VecDeque::new();
+        stack.push_back(starting_vertex.key().clone());
+
+        return if let Some((mut new_graph, _)) = new_graph.remove_all_edges() {
+            let successors = new_graph.successors_as_key_and_edges();
+            let mut flagged: HashSet<K> = HashSet::new();
+
+            flagged.insert(starting_vertex.key().clone());
+
+            while !stack.is_empty() {
+                let current = stack.pop_back().unwrap();
+                let neighbours = successors.get(&current).unwrap();
+
+                for neighbour in neighbours {
+                    if !flagged.contains(neighbour.to()) {
+                        new_graph = new_graph.add_edge(*neighbour).unwrap();
+                        flagged.insert(neighbour.to().clone());
+                        stack.push_back(neighbour.to().clone());
+                    }
+                }
+            }
+            Some(new_graph)
+        } else {
+            return None;
+        };
+    }
 }
