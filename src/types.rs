772184b699fb8fd9a33f19b6bb7a601dfbbae1ff
Haxos <a.mayocartes@protonmail.com>
2021-02-16 21:44:39 +0100

    fixed BDN PartialEq

--- src/types.rs
+++ src/types.rs
@@ -10,8 +10,8 @@ pub trait Value: Clone + Copy + Default + PartialEq {}
 impl<T: Copy + Default + PartialEq> Value for T {}
 
 /// An interface used as an helper to implement a weight for an edge.
-pub trait Weight: Clone + Copy + Default + PartialEq + Eq + PartialOrd + Ord {}
-impl<T: Copy + Default + Ord> Weight for T {}
+pub trait Weight: Clone + Copy + Default + PartialEq + PartialOrd {}
+impl<T: Copy + Default + PartialEq + PartialOrd> Weight for T {}
 
 /// A structure describing a vertex with a [`Key`] and a [`Value`].
 #[derive(Clone, Copy, Debug)]
@@ -35,13 +35,14 @@ where
 }
 
 /// A structure describing an edge with an origin [`Key`], a destination [`Key`] and a [`Weight`].
-#[derive(Clone, Copy, Hash, Eq, PartialEq, Ord, PartialOrd, Debug)]
+#[derive(Clone, Copy, Debug)]
 pub struct WeightedEdge<K, W>
 where
     K: Key,
     W: Weight,
 {
-    edge: Edge<K>,
+    from: K,
+    to: K,
     weight: W,
 }
 
@@ -146,6 +147,66 @@ where
     }
 }
 
+impl<K, W> Hash for WeightedEdge<K, W>
+where
+    K: Key,
+    W: Weight,
+{
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.from.hash(state);
+        self.to.hash(state);
+    }
+}
+
+impl<K, W> PartialEq for WeightedEdge<K, W>
+where
+    K: Key,
+    W: Weight,
+{
+    fn eq(&self, other: &Self) -> bool {
+        self.from.eq(&other.from) && self.to.eq(&other.to)
+    }
+}
+
+impl<K, W> Eq for WeightedEdge<K, W>
+where
+    K: Key,
+    W: Weight,
+{
+}
+
+impl<K, W> PartialOrd for WeightedEdge<K, W>
+where
+    K: Key,
+    W: Weight,
+{
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        if self.from.lt(&other.from) || self.from.eq(&other.from) && self.to.lt(&other.to) {
+            Some(Ordering::Less)
+        } else if self.from.eq(&other.from) && self.to.eq(&other.to) {
+            Some(Ordering::Equal)
+        } else {
+            Some(Ordering::Greater)
+        }
+    }
+}
+
+impl<K, W> Ord for WeightedEdge<K, W>
+where
+    K: Key,
+    W: Weight,
+{
+    fn cmp(&self, other: &Self) -> Ordering {
+        if self.from.lt(&other.from) || self.from.eq(&other.from) && self.to.lt(&other.to) {
+            Ordering::Less
+        } else if self.from.eq(&other.from) && self.to.eq(&other.to) {
+            Ordering::Equal
+        } else {
+            Ordering::Greater
+        }
+    }
+}
+
 impl<K, W> WeightedEdge<K, W>
 where
     K: Key,
@@ -154,27 +215,25 @@ where
     /// Create a new edge.
     pub fn new(from: K, to: K) -> Self {
         WeightedEdge {
-            edge: Edge::new(from, to),
+            from,
+            to,
             weight: W::default(),
         }
     }
 
     /// Create a new edge with a weight.
     pub fn with_weight(from: K, to: K, weight: W) -> Self {
-        WeightedEdge {
-            edge: Edge::new(from, to),
-            weight,
-        }
+        WeightedEdge { from, to, weight }
     }
 
     /// Get the origin.
     pub fn from(&self) -> &K {
-        &self.edge.from()
+        &self.from
     }
 
     /// Get the destination.
     pub fn to(&self) -> &K {
-        &self.edge.to()
+        &self.to
     }
 
     /// Get the weight.
@@ -184,6 +243,6 @@ where
 
     /// Convert to a weightless edge.
     pub fn to_edge(&self) -> Edge<K> {
-        self.edge.clone()
+        Edge::new(self.from.clone(), self.to.clone())
     }
 }
